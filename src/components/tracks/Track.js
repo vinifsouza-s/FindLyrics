214eabab798b1bf88fc04a49740fb427bf7647f3
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-09-13 12:31:54 -0300

    Integração de dados da API em tela

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Track= (props) => {
+    const {track} = props;
+
+    return (
+        <div className="track">
+            <div className="trackid">
+                <div className="trackbody"> 
+                    <h5> {track.artist_name} </h5>
+                    <p className="card-text">
+                        <strong>Track</strong> : {track.track_name}
+                        <br/>
+                        <strong>Album</strong> : {track.album_name}
+                    </p>
+                    <Link to={`lyrics/track/${track.track_id}`} className="trackbtn">
+                        View Lyrics
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Track;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Consumer } from '../../context';
 import Spinner from '../layout/Spinner';
+import Track from './Track';
 
  const Tracks = () => {
     return (
         <Consumer>
             {value => {
-                const { track_list } = value
-                if(track_list === undefined || track_list.length === 0){
-                    return <Spinner />
+                const { track_list , heading} = value
+                if (track_list === undefined || track_list.length === 0){
+                    return <Spinner />;
                 } else {
-                    return <h1>Tracks Loaded</h1>
+                    return (
+                        <>
+                        <h3 className="text-center"> {heading} </h3>
+                            <div className="row">
+                                {track_list.map(item => (
+                                    <Track key={item.track.track_id} track={item.track} />
+                                ))}
+                            </div>
+                        </>
+                    )
                 }
             }}
         </Consumer>
