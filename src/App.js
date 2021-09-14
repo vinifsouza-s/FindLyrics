9aefe4d833f2143318adc77e003b11b0e9a781aa
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-09-14 12:14:24 -0300

    Criada rota de informações de musica

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Index from './components/layout/Index';
 import Navbar from "./components/layout/Navbar";
+import Lyrics from './components/tracks/Lyrics';
+
 import { Provider } from './context';
 import './index.css';
+
 
 function App() {
   return (
@@ -15,6 +18,7 @@ function App() {
                 <div className="container">
                   <Switch>
                     <Route exact path="/" component={Index} />
+                    <Route exact path="/lyrics/track/:id" component={Lyrics} />
                   </Switch>
                 </div>
             </header>
